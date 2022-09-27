6eff888ec85f16707d30368e81813124b58c1912
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-09-27 13:17:25 +0300

    Done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,11 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
+  def show
+    #find a cheese using the id from url
+    cheese = Cheese.find(params[:id])
+    #send a json response using that cheese object
+    render json: cheese
+  end
 
 end
